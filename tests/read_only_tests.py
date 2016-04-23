6f061e444c6f2e4a5c3479281b15a5d7cb302107
Marco 'don' Kaulea <DonMarco42@gmail.com>
2016-04-23 11:47:49 +0200

    Add tests for user limits

--- tests/read_only_tests.py
+++ tests/read_only_tests.py
@@ -4,9 +4,11 @@ import requests
 
 URL = ("http://", "127.0.0.1", ":", "8000", "/")
 
+HEADERS = {'Content-Type': 'application/json'}
+
 
 def test_empty_user_list():
-    r = requests.get(''.join(URL + ('user', )))
+    r = requests.get(''.join(URL + ('user',)))
     assert r.status_code == 200
     assert r.headers['Content-Type'] == 'application/json'
     users = json.loads(r.text)
@@ -43,6 +45,34 @@ def test_empty_transactions():
     assert {'overall_count', 'limit', 'offset', 'entries'}.issubset(transactions)
     assert transactions['overall_count'] == 0
     assert transactions['limit'] == 100
+    assert transactions['offset'] == 0
+    assert transactions['entries'] == []
+
+
+def test_user_limits():
+    r = requests.get(''.join(URL + ('user',)),
+                     headers=HEADERS,
+                     params={'offset': 1, 'limit': 1})
+    assert r.status_code == 200
+    assert r.headers['Content-Type'] == 'application/json'
+    transactions = json.loads(r.text)
+    assert {'overall_count', 'limit', 'offset', 'entries'}.issubset(transactions)
+    assert transactions['overall_count'] == 0
+    assert transactions['limit'] == 1
+    assert transactions['offset'] == 1
+    assert transactions['entries'] == []
+
+
+def test_user_limits_max():
+    r = requests.get(''.join(URL + ('user',)),
+                     headers=HEADERS,
+                     params={'offset': 0, 'limit': 1000})
+    assert r.status_code == 200
+    assert r.headers['Content-Type'] == 'application/json'
+    transactions = json.loads(r.text)
+    assert {'overall_count', 'limit', 'offset', 'entries'}.issubset(transactions)
+    assert transactions['overall_count'] == 0
+    assert transactions['limit'] == 250  # 250 is the max
     assert transactions['offset'] == 0
     assert transactions['entries'] == []
 
