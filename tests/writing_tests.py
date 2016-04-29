71dc613cf61395e4bf283d39e287e5a9f5b93c94
Marco 'don' Kaulea <DonMarco42@gmail.com>
2016-04-29 18:55:20 +0200

    Test detecting invalid values

--- tests/writing_tests.py
+++ tests/writing_tests.py
@@ -77,3 +77,31 @@ class WritingTests(unittest.TestCase):
         transactions = json.loads(r.text)
         self.assertTrue({'overall_count', 'limit', 'offset', 'entries'}.issubset(transactions))
         self.assertEqual({'overall_count': 0, 'limit': 100, 'offset': 0, 'entries': []}, transactions)
+
+    def test_04_create_transaction_fail_nan(self):
+        # Fail to create transaction when value is not a number
+        params = {'name': 'gert', 'mail_address': 'gertMail'}
+        r = requests.post(''.join(URL + ('user/',)), headers=HEADERS, data=json.dumps(params))
+        self.assertEqual(201, r.status_code, msg=r.text)
+        self.assertEqual('application/json', r.headers['Content-Type'])
+
+        result = json.loads(r.text)
+        self.assertTrue({'id',
+                         'name',
+                         'mail_address',
+                         'balance',
+                         'last_transaction'}.issubset(result))
+        self.assertEqual('gert', result['name'])
+        self.assertEqual(int, type(result['id']))
+        self.assertEqual(0, result['balance'])
+        self.assertEqual(None, result['last_transaction'])
+        params = {'value': 'foo'}
+        r = requests.post(''.join((URL + ('user', '/', str(result['id']), '/', 'transaction', '/'))),
+                          headers=HEADERS,
+                          data=json.dumps(params))
+        self.assertEqual(400, r.status_code)
+        assert r.headers['Content-Type'] == 'application/json'
+        result = json.loads(r.text)
+        assert 'value' in result
+        assert result['value'] == ['A valid integer is required.']
+
