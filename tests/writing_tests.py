9b7dc203f2b1dc28afdf6ccf4e737b7343bf81e4
Marco 'don' Kaulea <DonMarco42@gmail.com>
2016-05-04 21:47:59 +0200

    Test transaction/account limits

--- tests/writing_tests.py
+++ tests/writing_tests.py
@@ -22,10 +22,10 @@ class UserCreationTests(unittest.TestCase):
 
         result = json.loads(r.text)
         self.assertTrue({'id',
-                'name',
-                'mail_address',
-                'balance',
-                'last_transaction'}.issubset(result))
+                         'name',
+                         'mail_address',
+                         'balance',
+                         'last_transaction'}.issubset(result))
         self.assertEqual('gert', result['name'])
         self.assertEqual(int, type(result['id']))
         self.assertEqual(0, result['balance'])
@@ -146,9 +146,59 @@ class TransactionCreationTests(unittest.TestCase):
         self.assertGreater(20, (now - create_date).total_seconds())
         self.assert_user(self.user, (1100, 1201))
 
+    def test_08_create_transaction_fail_lower_account_boundary(self):
+        # Fail to create transaction with 403 (lower account boundary)
+        params = {'value': -10000}
+        r = requests.post(''.join((URL + ('user', '/', self.user, '/', 'transaction', '/'))),
+                          headers=HEADERS,
+                          data=json.dumps(params))
+        self.assertEqual(403, r.status_code)
+        self.assertEqual('application/json', r.headers['Content-Type'])
+        result = json.loads(r.text)
+        expected_message = "transaction value of -10000 leads to an overall account balance " \
+                           "of -10000 which goes below the lower account limit of -2300"
+        self.assertEqual(expected_message, result.get('msg'))
+
+    def test_09_create_transaction_fail_upper_account_boundary(self):
+        # Fail to create transaction with 403 (lower account boundary)
+        params = {'value': 10000}
+        r = requests.post(''.join((URL + ('user', '/', self.user, '/', 'transaction', '/'))),
+                          headers=HEADERS,
+                          data=json.dumps(params))
+        self.assertEqual(403, r.status_code)
+        self.assertEqual('application/json', r.headers['Content-Type'])
+        result = json.loads(r.text)
+        expected_message = "transaction value of 10000 leads to an overall account balance of 10000 " \
+                           "which goes beyond the upper account limit of 4200"
+        self.assertEqual(expected_message, result.get('msg'))
+
+    def test_10_create_transaction_fail_lower_transaction_boundary(self):
+        # Fail to create transaction with 403 (lower account boundary)
+        params = {'value': -9999999}
+        r = requests.post(''.join((URL + ('user', '/', self.user, '/', 'transaction', '/'))),
+                          headers=HEADERS,
+                          data=json.dumps(params))
+        self.assertEqual(403, r.status_code)
+        self.assertEqual('application/json', r.headers['Content-Type'])
+        result = json.loads(r.text)
+        expected_message = "transaction value of -9999999 falls below the transaction minimum of -999900"
+        self.assertEqual(expected_message, result.get('msg'))
+
+    def test_11_create_transaction_fail_upper_transaction_boundary(self):
+        # Fail to create transaction with 403 (lower account boundary)
+        params = {'value': 9999999}
+        r = requests.post(''.join((URL + ('user', '/', self.user, '/', 'transaction', '/'))),
+                          headers=HEADERS,
+                          data=json.dumps(params))
+        self.assertEqual(403, r.status_code)
+        self.assertEqual('application/json', r.headers['Content-Type'])
+        result = json.loads(r.text)
+        expected_message = "transaction value of 9999999 exceeds the transaction maximum of 999900"
+        self.assertEqual(expected_message, result.get('msg'))
+
     def test_12_invalid_json(self):
         # Fail to create transaction with 403 (lower account boundary)
-        r = requests.post(''.join((URL + ('user', '/', '1', '/', 'transaction', '/'))),
+        r = requests.post(''.join((URL + ('user', '/', self.user, '/', 'transaction', '/'))),
                           headers=HEADERS,
                           data='{"name":}')
         print(r.text)
